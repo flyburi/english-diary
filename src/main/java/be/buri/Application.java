fc311bae0ccd45c17f65f8cec56543814dd4563d
buri <buri@buriui-MacBook-Air.local>
2015-04-05 18:59:43 +0900

    -Added extends SpringBootServletInitializer

--- src/main/java/be/buri/Application.java
+++ src/main/java/be/buri/Application.java
@@ -3,11 +3,18 @@ package be.buri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
   }
 }
